83f311378d45fb3ca1f21fb534398094f80c2af3
0xev <ev_evidence@proton.me>
2023-02-20 17:38:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+      <div className="text">
+        <h1>Learn to code by watching others</h1>
+        <p className="title">
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className="form-and-buttons">
+        <button className="try-btn">
+          <span>Try it free 7 days</span> then $20/mo. thereafter
+        </button>
+        <form className="card">
+          <input type="text" placeholder="First Name" required />
+          <input type="text" placeholder="Last Name" required />
+          <input type="text" placeholder="Email Address" required />
+          <input type="text" placeholder="Password" required />
+          <button className="claim-btn">CLAIM YOUR FREE TRIAL</button>
+          <p>
+            By clicking the button, you are agreeing to our
+            <a href="www.google.com">Terms and Services</a>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
